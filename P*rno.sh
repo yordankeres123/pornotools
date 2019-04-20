a9a7af0dbc2115b5672cb432c825294e48b591c6
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-04-20 13:41:39 +0700

    Add files via upload

---
+++ P*rno.sh
@@ -0,0 +1,24 @@
+clear
+figlet PORNO.COM
+figlet INGAT DOSA
+
+echo "=======================" | lolcat
+echo
+echo "Dosa Tanggung Sendiri yah cuk"
+echo
+echo "BIGBOS STARLA"
+echo
+echo "#######################"
+echo
+echo "CYB3R POP1CE +62"
+echo
+echo "#######################" | lolcat
+echo
+echo "jangan dilanjutin cok!!!"
+echo
+echo "=======================" | lolcat
+
+figlet CIEEEE
+figlet OTAK BOKEP
+figlet CARI CARI
+figlet BUAT BACOL
